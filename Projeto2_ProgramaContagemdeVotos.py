6cc064854ef88cf921437f85e90c30eace6d5d2e
Gabriel <pepece.gabriel@gmail.com>
2024-03-14 20:33:19 -0300

    Projeto2_ProgramaContagemdeVotos.py

---
+++ Projeto2_ProgramaContagemdeVotos.py
@@ -0,0 +1,32 @@
+#Primeiro passo, definir as variáveis contadoras 
+votos_candidato1 = 0
+votos_candidato2 = 0
+votos_candidato3 = 0
+votos_candidato4 = 0
+votos_nulos = 0
+votos_brancos = 0
+
+#Criação do início do laço para leitura de votos
+for i in range(0,20) :
+    voto = int(input('informe seu voto: '))
+    if voto == 1:
+        votos_candidato1 += 1
+    elif voto == 2:
+        votos_candidato2 += 1
+    elif voto == 3:
+        votos_candidato3 += 1
+    elif voto == 4:
+        votos_candidato4 += 1
+    elif voto == 5:
+        votos_nulos += 1
+    elif voto == 6:
+        votos_brancos +=1
+    else:
+        print('Voto innválido')
+
+print(f'Votos candidato 1: {votos_candidato1}')
+print(f'Votos candidato 2: {votos_candidato2}')
+print(f'Votos candidato 3: {votos_candidato3}')
+print(f'Votos candidato 4: {votos_candidato4}')
+print(f'Votos nulos: {votos_nulos}')
+print(f'Votos brancos: {votos_brancos}')
